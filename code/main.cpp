f38eb1357b500a0bfc1c48c31cb7dddd730f05fd
Vedad Hodzic <vedadh@student.matnat.uio.no>
2013-11-29 18:06:29 +0100

    Writing info to file.

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "SolarSystem.hpp"
 #include <ctime>
+#include <fstream>
 
 using namespace std;
 
@@ -12,9 +13,14 @@ int main(int argc, char* argv[]) {
 	SolarSystem myCluster = SolarSystem(100, 20);
 //	mySystem.setCenterOfMassPosition();
 //	mySystem.setTotalMomentum();
-	myCluster.systemSimulation(0.01, 2, false, false, "RK4");
+	myCluster.systemSimulation(0.01, 5, true, false, "leapfrog");
 	finish = clock();
 	cout << "Computation time: " << double(finish - start)/CLOCKS_PER_SEC << " seconds" << endl;
+
+	fstream outFile;
+	outFile.open("../data/info.dat", ios::out);
+	outFile << 100 << " " << 20 << " " << 0.01 << " " << 5 << endl;
+	outFile.close();
 
 	return 0;
 }
